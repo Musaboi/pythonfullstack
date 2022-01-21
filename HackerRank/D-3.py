48a9ab123db0ba406b6dcd8fd2f84ab990f34cc7
MusaSudad <musasudad271@gmail.com>
2022-01-22 00:32:54 +0600

    D-3.py finished

--- HackerRank/D-3.py
+++ HackerRank/D-3.py
@@ -1,1 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+N = int(input())
+
+if N%2 != 0:
+    print("Weird")
+elif N%2 == 0 & 2 <= N <= 5:
+    print("Not Weird")
+elif N%2 ==0 & 6 <= N <= 20:
+    print("Weird")
+elif N%2 ==0 & 20<N:
+    print("Not Weird")
+
 
