8e680cdaf5b55e07b57bc1ee2a39cbff8e2e6267
MusaSudad <musasudad271@gmail.com>
2022-01-22 00:08:29 +0600

    Day2Opt.py finished

--- HackerRank/Day2Opt.py
+++ HackerRank/Day2Opt.py
@@ -19,7 +19,8 @@ meal_cost = float(input())
 tip_percent = int(input())
 tax_percent = int(input())
 def solve(meal_cost,tip_percent, tax_percent):
-    print(int(meal_cost+tip_percent/100*meal_cost+tax_percent/100*meal_cost))
+    total_cost = meal_cost+tip_percent/100*meal_cost+tax_percent/100*meal_cost
+    print(round(total_cost))
 
 solve(meal_cost,tip_percent,tax_percent)
 
